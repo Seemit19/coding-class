df11d6631189ebd3c5265f08a4180b9494838637
Seemit Prajapat <119443065+Seemit19@users.noreply.github.com>
2023-05-01 19:27:50 +0530

    Update pattern1.c

--- pattern/pattern1.c
+++ pattern/pattern1.c
@@ -5,7 +5,7 @@
 
 
 
-
+//using if condition.
 #include<stdio.h>
 #include<conio.h>
 main()
@@ -15,6 +15,22 @@ main()
     {
         for(c=1;c<=4;c++)
         if(c<=r)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+/*without using if condition*/ 
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int r,c;
+    for(r=1;r<=4;r++)
+    {
+        for(c=1;c<=r;c++)
         {
             printf("*");
         }
