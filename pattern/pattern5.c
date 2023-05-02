fe73de74655e7b2e4d36751d19c9d6a9fca2f5ce
Seemit Prajapat <119443065+Seemit19@users.noreply.github.com>
2023-05-02 15:41:16 +0530

    Update pattern5.c

--- pattern/pattern5.c
+++ pattern/pattern5.c
@@ -6,7 +6,7 @@
 ******
 
 
-//using for loop 
+//using for loop with if condition 
 #include<stdio.h>
 #include<conio.h>
 main()
@@ -21,6 +21,24 @@ main()
             else
             printf(" ");
         }
+        printf("\n");
+    }
+}
+
+//using for loop without if conditoin
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int i,r,c,n;
+    printf("input the range upto you want pattern\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(r=1;r<=(n-i);r++)
+        {printf(" ");}
+        for(c=1;c<=i;c++)
+        {printf("*");}
         printf("\n");
     }
 }
