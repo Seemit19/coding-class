cf60b300831ec31898d0d408a8f094dfb6a0a2db
Seemit Prajapat <119443065+Seemit19@users.noreply.github.com>
2023-05-03 15:32:11 +0530

    Create pattern6.c

---
+++ pattern/pattern6.c
@@ -0,0 +1,21 @@
+55555
+4444
+333
+22
+1
+
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int n=5,a,i;
+    for(a=1;a<=5;a++)
+    {
+        for(i=1;i<=n;i++)
+       {
+           printf("%d",n);
+       }n=n-1;
+       printf("\n");
+    }
+}
