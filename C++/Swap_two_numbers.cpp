57295a0ab938d3a8fbe005cec11041ac67c741d9
Seemit Prajapat <119443065+Seemit19@users.noreply.github.com>
2023-05-07 11:24:25 +0530

    Create Swap_two_numbers.cpp

---
+++ C++/Swap_two_numbers.cpp
@@ -0,0 +1,25 @@
+//Swapping two numbers using third variable
+ 
+ 
+#include<iostream>
+
+using namespace std;
+
+ main()
+{
+    float a,b;
+
+    cout<<"Enter two number a = ";
+    cin>>a;
+    cout<<"b = ";
+    cin>>b;
+
+    if (a==b)
+    {
+        cout<<endl <<"----Numbers are equal----" <<endl <<endl;
+    }
+    else
+    {
+        cout<<endl <<"----Numbers are not equal----" <<endl <<endl;
+    }
+}
