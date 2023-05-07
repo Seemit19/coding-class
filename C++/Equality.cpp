4379142bf2174ae507c0dead40a9665b42f19c4b
Seemit Prajapat <119443065+Seemit19@users.noreply.github.com>
2023-05-07 11:30:50 +0530

    Create Equality.cpp

---
+++ C++/Equality.cpp
@@ -0,0 +1,26 @@
+//Cheking whether two numbers are equal or not
+ 
+ 
+ 
+#include<iostream>
+
+using namespace std;
+
+ main()
+{
+    float a,b;
+
+    cout<<"Enter two number a = ";
+    cin>>a;
+    cout<<"b = ";
+    cin>>b;
+
+    if (a==b)
+    {
+        cout<<endl <<"----Numbers are equal----" <<endl <<endl;
+    }
+    else
+    {
+        cout<<endl <<"----Numbers are not equal----" <<endl <<endl;
+    }
+}
